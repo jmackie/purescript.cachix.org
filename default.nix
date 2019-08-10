bac397a2a6aaa8c845781fb1ec87cbd555847a1a
Jordan Mackie <jmackie@protonmail.com>
2019-08-10 16:47:04 +0100

    Fix purty build (hopefully)

--- default.nix
+++ default.nix
@@ -16,14 +16,24 @@ let
 
   patchSpago = import ./spago/patch.nix { inherit pkgs; };
 
-  # https://github.com/input-output-hk/haskell.nix/issues/214#issuecomment-515785254
-  mkStackPkgSet = pkgs-nix:
+  mkStackPkgSet = extras: pkgs-nix:
     haskell.mkStackPkgSet {
       stack-pkgs = pkgs-nix;
       pkg-def-extras =
-        [ (hackage: { hsc2hs = hackage.hsc2hs."0.68.4".revisions.default; }) ];
+
+        # https://github.com/input-output-hk/haskell.nix/issues/214#issuecomment-515785254
+        [ (hackage: { hsc2hs = hackage.hsc2hs."0.68.4".revisions.default; }) ]
+        ++ extras;
       modules = [{ doHaddock = false; }];
     };
+
+  mkPursPkgSet = mkStackPkgSet [ ];
+  mkSpagoPkgSet = mkStackPkgSet [ ];
+  mkPurtyPkgSet = mkStackPkgSet [
+    # https://stackoverflow.com/questions/53546699/building-contravariant-using-stack-leads-to-constraint-error-about-not-being-abl
+    # https://hub.darcs.net/ross/transformers/patch/b00658faa4578b0eb578cf158c75185c1c0ec697
+    (hackage: { transformers = hackage.hsc2hs."0.5.6.2".revisions.default; })
+  ];
 
   exe = package: name: pkgSet:
     pkgSet.config.hsPkgs.${package}.components.exes.${name};
@@ -31,29 +41,29 @@ let
 in {
   purs = let pursExe = exe "purescript" "purs";
     in {
-      v0_13_0 = pursExe (mkStackPkgSet (import ./purs/v0.13.0/pkgs.nix));
-      v0_13_2 = pursExe (mkStackPkgSet (import ./purs/v0.13.2/pkgs.nix));
+      v0_13_0 = pursExe (mkPursPkgSet (import ./purs/v0.13.0/pkgs.nix));
+      v0_13_2 = pursExe (mkPursPkgSet (import ./purs/v0.13.2/pkgs.nix));
     };
 
   spago = let spagoExe = exe "spago" "spago";
     in {
       v0_9_0_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.9.0.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.9.0.0/pkgs.nix)));
       v0_8_5_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.8.5.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.8.5.0/pkgs.nix)));
       v0_8_4_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.8.4.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.8.4.0/pkgs.nix)));
       v0_8_3_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.8.3.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.8.3.0/pkgs.nix)));
       v0_8_1_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.8.1.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.8.1.0/pkgs.nix)));
       v0_8_0_0 =
-        spagoExe (mkStackPkgSet (patchSpago (import ./spago/0.8.0.0/pkgs.nix)));
+        spagoExe (mkSpagoPkgSet (patchSpago (import ./spago/0.8.0.0/pkgs.nix)));
     };
 
   purty = let purtyExe = exe "purty" "purty";
     in {
-      v4_5_1 = purtyExe (mkStackPkgSet (import ./purty/4.5.1/pkgs.nix));
-      v4_5_0 = purtyExe (mkStackPkgSet (import ./purty/4.5.0/pkgs.nix));
+      v4_5_1 = purtyExe (mkPurtyPkgSet (import ./purty/4.5.1/pkgs.nix));
+      v4_5_0 = purtyExe (mkPurtyPkgSet (import ./purty/4.5.0/pkgs.nix));
     };
 }
